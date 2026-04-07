b37c321ee5bac21a5098923f80ac699f77b243d1
TerryBlackhoodWoo <leftdeadman@gmail.com>
2026-04-07 16:18:12 +0900

    chore: 개인 작업 폴더 PR에서 제외

--- TERRY/p01_backEnd/DAO/fable/fableStringCleaner.py
+++
@@ -1,7 +0,0 @@
-class fableStringCleaner:
-    @staticmethod
-    def clean(txt):
-        for t in ["<b>", "</b>", "&gt;", "&lt;", "&quot;"]:
-            txt = txt.replace(t, " ")
-        return txt
-    
